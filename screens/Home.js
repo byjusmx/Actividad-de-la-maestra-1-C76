e3d0b968f372876b297a287e7f215ad40898d903
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-10-18 03:48:57 -0500

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -10,7 +10,7 @@ export default class HomeScreen extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>Home Screen!</Text>
+                <Text>¡Pantalla de inicio!</Text>
             </View>
         )
     }
