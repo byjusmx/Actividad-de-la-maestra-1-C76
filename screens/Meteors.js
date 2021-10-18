367dcf0ca150d720842a03cde3462e8e31698c44
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-10-18 03:50:05 -0500

    Update Meteors.js

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -10,7 +10,7 @@ export default class MeteorScreen extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>Meteor Screen!</Text>
+                <Text>Pantalla de Meteoros!</Text>
             </View>
         )
     }
